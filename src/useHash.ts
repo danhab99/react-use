936e46e2fa5569a696e45da910225321267315a7
Dan Habot <dan.habot@gmail.com>
2025-01-08 11:16:11 -0500

    fix: check if window is undefined

--- src/useHash.ts
+++ src/useHash.ts
@@ -1,33 +1,41 @@
-import { useCallback, useState } from 'react';
-import useLifecycles from './useLifecycles';
-import { off, on } from './misc/util';
+import { useCallback, useState } from "react";
+import useLifecycles from "./useLifecycles";
+import { off, on } from "./misc/util";
 
 /**
  * read and write url hash, response to url hash change
  */
 export const useHash = () => {
-  const [hash, setHash] = useState(() => window.location.hash);
+  const [hash, setHash] = useState(() =>
+    typeof window !== "undefined" ? window.location.hash : "",
+  );
 
   const onHashChange = useCallback(() => {
-    setHash(window.location.hash);
+    if (typeof window !== "undefined") {
+      setHash(window.location.hash);
+    }
   }, []);
 
   useLifecycles(
     () => {
-      on(window, 'hashchange', onHashChange);
+      if (typeof window !== "undefined") {
+        on(window, "hashchange", onHashChange);
+      }
     },
     () => {
-      off(window, 'hashchange', onHashChange);
-    }
+      if (typeof window !== "undefined") {
+        off(window, "hashchange", onHashChange);
+      }
+    },
   );
 
   const _setHash = useCallback(
     (newHash: string) => {
-      if (newHash !== hash) {
+      if (typeof window !== "undefined" && newHash !== hash) {
         window.location.hash = newHash;
       }
     },
-    [hash]
+    [hash],
   );
 
   return [hash, _setHash] as const;
